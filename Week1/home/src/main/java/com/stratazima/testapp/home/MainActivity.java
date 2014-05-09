ead3bb58551c992c77833aa94bbe90df0b4eb758
xiaoxion <esaurubio11@gmail.com>
2014-05-09 19:42:27 -0400

    Add some initial framework
    This is my first addition to the project

---
+++ Week1/home/src/main/java/com/stratazima/testapp/home/MainActivity.java
@@ -0,0 +1,54 @@
+package com.stratazima.testapp.home;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import static android.view.View.*;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    // Variable Declarations
+    int daInteger;
+    boolean daBoolean;
+    String daString;
+    String[] daStringArray;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.go_button);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+                daString = "Tell me dat I'm pretty";
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
